aaaf200efd24abd8c06b608b9ff749819b598f93
kevinsconnolly <66528100+kevinsconnolly@users.noreply.github.com>
2020-08-30 17:43:48 -0500

    Add files via upload

--- Earthquake_Challenge folder/static/js/logic.js
+++ Earthquake_Challenge folder/static/js/logic.js
@@ -1,54 +1,54 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// We create the tile layer that will be the background of our map.
+//tile layer background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
 });
-// We create the tile layer that will be an option for our map.
+// tile layer option for our map.
 let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
 });
-// We create the dark view tile layer that will be an option for our map.
+// dark view tile layer option for our map.
 let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
 });
 
-// Create the map object with center, zoom level and default layer.
+// map object with center, zoom level and default layer.
 let map = L.map('mapid', {
 	center: [39.5, -98.5],
 zoom: 3,
 	layers: [streets]
 })
 
-// Create a base layer that holds both maps.
+// base layer created that holds both maps.
 let baseMaps = {
 	"Streets": streets,
   "Satellite": satelliteStreets,
   "Dark": dark
   };
 
-// Create the earthquake layer for our map.
+// Create earthquake layer for our map.
 let earthquakes = new L.LayerGroup();
 
-// Create the tectonic layer for our map.
+// Create  tectonic layer for our map.
 let tectonic = new L.LayerGroup();
 
-// We define an object that contains the overlays.
-// This overlay will be visible all the time.
+// object defined which contains the overlays.
+// This overlay is always visible.
 let overlays = {
     Earthquakes: earthquakes,
     Tectonic: tectonic
   };
 
-// Then we add a control to the map that will allow the user to change
-// which layers are visible.
+// control added to the map that will allow the user to change
+
 L.control.layers(baseMaps, overlays).addTo(map);
 
 // Accessing the Tectonicplates GeoJSON URL.
@@ -70,7 +70,7 @@ d3.json(tectonicData).then(function(data) {
       // Then we add the tectonic layer to our map.
       tectonic.addTo(map);
 
-      // This function returns the style data for each of the earthquakes we plot on
+      // This function returns  style data for each of the earthquakes we plot on
 // the map. We pass the magnitude of the earthquake into two separate functions
 // to calculate the color and radius.
 function styleInfo(feature) {
@@ -124,16 +124,16 @@ L.geoJson(data, {
       console.log(data);
       return L.circleMarker(latlng);
     },
-  // We set the style for each circleMarker using our styleInfo function.
+  //style is set for each circleMarker using our styleInfo function.
 style: styleInfo,
-  // We create a popup for each circleMarker to display the magnitude and
+  // popup created for each circleMarker to display the magnitude and
   //  location of the earthquake after the marker has been created and styled.
   onEachFeature: function(feature, layer) {
   layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
 }
 }).addTo(earthquakes);
 
-  // Then we add the earthquake layer to our map.
+  // Add the earthquake layer to our map.
   earthquakes.addTo(map);
   
   
@@ -141,7 +141,7 @@ style: styleInfo,
   var legend = L.control({
       position: 'bottomright'
   });
-  // Then add all the details for the legend.
+  // Add all the details for the legend.
   legend.onAdd = function () {
       
       var div = L.DomUtil.create('div', 'info legend');
@@ -155,7 +155,7 @@ style: styleInfo,
           "#ea2c2c"
       ];
       
-      // Looping through our intervals to generate a label with a colored square for each interval.
+      // Loop through our intervals; generate a label with a colored square for each interval.
       for (var i = 0; i < magnitudes.length; i++) {
           console.log(colors[i]);
           div.innerHTML +=
